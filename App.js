6d9fed9038c0f4266d841a0410dd7c860c723ab9
Jérôme Commaret <jcommaret@gmail.com>
2023-12-15 04:54:55 +0100

    Working navigation

--- App.js
+++ App.js
@@ -1,25 +1,20 @@
 // In App.js in a new project
 
 import * as React from "react";
-import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import { HomeScreen } from "./src/screen/HomeScreen";
+import { DetailsScreen } from "./src/screen/DetailsScreen";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
