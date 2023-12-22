fda30a09c52feb20a70d210e36657d1811cbb734
Jérôme Commaret <jcommaret@gmail.com>
2023-12-22 23:33:18 +0100

    Changed path to json file to have it working

--- src/service/index.js
+++ src/service/index.js
@@ -1,6 +1,7 @@
 /** This file is used to define getData function that will be used in the application **/
 /** API_URL LOCALHOST, change this when in production */
-const API_URL = "../../assets/data/sample/data.json";
+const API_URL =
+  "https://raw.githubusercontent.com/jcommaret/GDEF/master/assets/data/est/data.json";
 
 export function getData() {
   return fetch(`${API_URL}`).then(async (res) => await res.json());
--- src/service/service.js
+++ src/service/service.js
@@ -2,7 +2,7 @@ import { getData } from ".";
 
 export async function getWords() {
   return getData().then(function (data) {
-    const wordsInfos = data;
+    const wordsInfos = data.volume.contribution;
     return wordsInfos;
   });
 }
